f5cade6506789a9752e0c6415acccb7af252ae8c
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-05-15 12:22:33 +0200

    Update 80percentProbeExtractor.py

--- python file/80percentProbeExtractor.py
+++ python file/80percentProbeExtractor.py
@@ -9,17 +9,19 @@ def extract_time(json):
     try:
         # Also convert to int since update_time will be string.  When comparing
         # strings, "10" is smaller than "2".
-        return int(json['timestamp'])
+        print((json['timestamp']))
+        return(datetime.strptime(json['timestamp'], '%Y-%m-%d %H:%M:%S'))
     except KeyError:
         return 0
 
 
-with open('C:/Users/guazz/Desktop/JSON_Elab/1666014.json', 'r') as unordered, open('C:/Users/guazz/Desktop/JSON_Elab/out.json', 'a') as out:
+with open('C:/Users/guazz/Desktop/JSON_Elab/test_in.json', 'r') as unordered, open('C:/Users/guazz/Desktop/JSON_Elab/out.json', 'a') as out:
 	lines = []
 	for line in unordered:
 	    json_obj = json.loads(line)
 	    lines.append(json_obj)
 	lines.sort(key=extract_time)
-	for li in lines:
-		out.write(json.dumps(li))
-		out.write('\n')
+	#ok si li ordina. ora vanno divisi in bin e poi fatti i count.
+	#for li in lines:
+	#	out.write(json.dumps(li))
+	#	out.write('\n')
