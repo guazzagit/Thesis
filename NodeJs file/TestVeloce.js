f44ccc332eda0c86ca4a2eaa1a1696c36a5899e8
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-01 21:38:22 +0200

    up

--- NodeJs file/TestVeloce.js
+++ NodeJs file/TestVeloce.js
@@ -12,24 +12,42 @@ console.log("Opening a new file..");
 console.log("Start append");
 // c'è il campo resultset devi sta attento a quello qua 
 var str = fs.createReadStream(Input)
-//var str = fs.readFileSync(Input, 'utf8'); // This will block the event loop, not recommended for non-cli programs.
-//console.log('result read: ' + str);
-str.on('data', function (chunk) {
-    var value2= chunk.toString().replace(/\},\{"af"+/g,'}{"af"')
-	var value3= value2.replace(/\},\{"dst_name"+/g,'}{"dst_name"')
-	var value4= value3.replace(/\},\{"time"+/g,'}{"time"')
-	var stringArray = value4.split(",{"); // divide 
-	let text = stringArray.join('\n{')+"\n"; // new line and add the lost bracket
-	var text2=text.replace(/\}\{+/g,'},{')
-	console.log(text2);
-	var value = text2.substring(1, text2.length-1);
-	fs.appendFile(fd, value, function(err) {
+			.pipe(es.split())
+			.pipe(es.mapSync(function(line){
 
-	    return console.log(err);
-		}
-	);
+		        // pause the readstream
+		        str.pause();
 
-});
+		            var value2= line.toString().replace(/\},\{"af"+/g,'}{"af"')
+					var value3= value2.replace(/\},\{"dst_name"+/g,'}{"dst_name"')
+					var value4= value3.replace(/\},\{"time"+/g,'}{"time"')
+					var stringArray = value4.split(",{"); // divide 
+					let text = stringArray.join('\n{')+"\n"; // new line and add the lost bracket
+					var text2=text.replace(/\}\{+/g,'},{')
+					console.log(text2);
+					var value = text2.substring(1, text2.length-1);
+					fs.appendFile(fd, value, function(err) {
+
+					    return console.log(err);
+						}
+					);
+
+
+		        // process line here and call s.resume() when rdy
+		        // function below was for logging memory usage
+		       
+
+		        // resume the readstream, possibly from a callback
+		        str.resume();
+		    })
+		    .on('error', function(err){
+		        console.log('Error while reading file.', err);
+		    })
+		    .on('end', function(){
+		        console.log('Read entire file.')
+		    })
+		);
+
 //lineReader.eachLine(Input, function(line,last) {
 	//var value = line.replace(/[\[\]]+/g,'');  //remove brackets
 	//var value = line.substring(1, line.length-1);
