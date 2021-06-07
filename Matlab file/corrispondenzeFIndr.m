1ad3dca51a867f787eb3eeb107ea75be5c98aaa7
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-07 18:43:10 +0200

    update

--- Matlab file/corrispondenzeFIndr.m
+++ Matlab file/corrispondenzeFIndr.m
@@ -5,7 +5,7 @@ Country = readtable('ContryProbe.csv');
 [numRows,numCols] = size(Country)
 FileOut= split(param1,"_")
 fname = sprintf('Corrispondenza_%s_%s.mat', FileOut{1},FileOut{3});
-prb_id= unique(T{:,8})
+prb_id= unique(T{:,1})
 [Rows,Cols] = size(prb_id)
 for p=1:Rows
     for h=1:numRows;
@@ -13,6 +13,7 @@ for p=1:Rows
             Corri{p,1}=prb_id(p,1);
             valore = cellstr(Country{h,3});
             Corri{p,2}=valore{1,1};
+            break
         end
     end
 end
