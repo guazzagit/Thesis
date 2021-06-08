4bac27b364af386fbf4c7e4a1773f8207e4feb70
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-08 19:01:25 +0200

    up

---
+++ Matlab file/corrispondenzeFIndr_alternative.m
@@ -0,0 +1,19 @@
+%% associa regione al probe_id
+function[] = corrispondenzeFIndr(Param1)
+T = readtable(Param1); %% inserire qua il csv da caricare
+Country = readtable('ContryProbe.csv');
+[numRows,numCols] = size(Country)
+FileOut= split(param1,"_")
+fname = sprintf('Corrispondenza_%s_%s.mat', FileOut{1},FileOut{3});
+prb_id= unique(T{:,1})
+[Rows,Cols] = size(prb_id)
+for p=1:Rows
+    tappo=find(Country{:,2}==prb_id(p,1))
+
+    Corri{p,1}=prb_id(p,1);
+    valore = cellstr(Country{tappo,3});
+    Corri{p,2}=valore{1,1};
+
+end
+save([Filename],'close')
+end
