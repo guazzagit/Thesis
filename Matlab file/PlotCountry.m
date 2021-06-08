1cdc6075cfad72456a4c6f00164f5120cbd0125e
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-08 21:40:34 +0200

    Update PlotCountry.m

--- Matlab file/PlotCountry.m
+++ Matlab file/PlotCountry.m
@@ -82,7 +82,7 @@ for j = 1:Rows
         legend(Nations{j})
         set(gcf,'color','w');
         fname = sprintf('%s_%s_Median_%s', FileOut{1},FileOut{3},Nations{j});
-        export_fig(['C:/Users/guazz/Desktop/' fname], '-pdf');
+        export_fig(['/plot/' fname], '-pdf');
     end
     
 end
