29bca761a5946d4ebb0fa778b78117e63d2e2971
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-09 08:17:30 +0200

    Update testComd.m

--- Matlab file/testComd.m
+++ Matlab file/testComd.m
@@ -1,7 +1,10 @@
-function[] = TaskTest(param1, param2)
+function[] = TaskTest(param1)
    disp(param1);
-   disp(param2);
    value= split(param1,"_")
-   fname = sprintf('%s_%s_Median_All', value{1},value{3});
+   fname = sprintf('%s_%s_prova', value{1},value{3});
    disp(fname)
+    set(gcf, 'Visible', 'off');
+    figure('Visible', 'off')
+    scatter(1,1,'x');
+   export_fig(['/plot/' fname], '-pdf');
 end
