3e8520455f675c8a7ac4855bcc828e98aa7e63ff
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-09 08:47:40 +0200

    Update testComd.m

--- Matlab file/testComd.m
+++ Matlab file/testComd.m
@@ -7,5 +7,5 @@ function[] = TaskTest(param1)
     figure('Visible', 'off')
     scatter(1,1,'x');
     drawnow()
-   export_fig(['/home/guazzelli/disco/Thesis/Matlab file/plot' fname], '-pdf');
+   export_fig(['/home/guazzelli/disco/Thesis/Matlab file/plot/' fname], '-pdf');
 end
