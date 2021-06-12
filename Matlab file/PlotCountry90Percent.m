1ae6412d9d90998da4b98818803741762097f1c4
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-12 17:59:06 +0200

    Update PlotCountry90Percent.m

--- Matlab file/PlotCountry90Percent.m
+++ Matlab file/PlotCountry90Percent.m
@@ -5,54 +5,56 @@ load(param2) %% caricamento file di corrispondenza paesi probe
 G = findgroups(T{:,1});     
 Tc = splitapply( @(varargin) varargin, T, G);
 FileOut= split(param1,"_")
+FileOut2=split(FileOut{1},"/")
 %fname = sprintf('%s_%s_Median_', FileOut{1},FileOut{3});
 
 
-%% carica il file corrispondenza
 [numRows,numCols] = size(Tc)
-for p=1:numRows
-    for h=1:numRows;
-        if unique(Tc{p,1})==Corri{h,1}
-            Tc{h,5}=Corri{h,2};
-        end
-    end
-end
+%% carica il file corrispondenza non serve più questo
+
+%for p=1:numRows
+ %   for h=1:numRows;
+ %       if unique(Tc{p,1})==Corri{h,1}
+ %%           Tc{h,5}=Corri{h,2};
+  %      end
+ %   end
+%end
+%% cancella fin qui
 Nations={'ES','FR','IT','SE','DE'}
+Nations2=["ES" "FR" "IT" "SE" "DE"]
+Tc(:,5)=Corri(:,2) % forse cosi posso assegnare diretto le corrispondenze...
 
-Peso=[]
-Tempo=[]
-appoggio=[]
-tabella=cell(5,2)
-tatto=cell(5,1)
-
-for g=1:size(Nations,2)
+T=[]
+tabellaTempi=cell(5,1)
+tabellaPesi=cell(5,1)
+parfor (g=1:size(Nations,2),3)
+    singlerow=cell(1,2)
+    tabellaTempo=[]
+    tabellaPeso=[]
     for b=1:numRows
-        if cell2mat(Tc(b,5)) == Nations{1,g}
-            %%Peso=[Peso;cell2mat(Tc(b,23))];
-            %%Tempo=[Tempo;[Tc(b,15)]]
-            Peso=vertcat(Peso,cell2mat(Tc(b,3)))
-            %% la parte sotto va sistemata
-            appoggio=cell2table(Tc(b,2))
-            appoggio=table2cell(appoggio)
-            Tempo=cat(1,Tempo,appoggio{1,1})
-            appoggio=[]
+        
+        if cell2mat(Tc(b,5)) == Nations{g}
+
+            singlerow{1,1}=cell2mat(Tc(b,3))
+            singlerow{1,2}=Tc{b,2}
+            %appoggio=[]
+            tabellaPeso=[tabellaPeso;singlerow{1}]
+            tabellaTempo=[tabellaTempo;singlerow{2}]
         end
+
     end
-    tabella{g,1}=Peso
-    Peso=[];
-    tabella{g,2}=Tempo
-    Tempo=[]
+    tabellaPesi{g,:}=tabellaPeso
+    tabellaTempi{g,:}=tabellaTempo
+
 
 end
-
-
 
 
 %% non plotta tutto ma è follia farlo cosi tanto.
 [Rows,Cols] = size(tabella)
 for j = 1:Rows
-    if(~cellfun('isempty',tabella(j,1)))
-        tabx = table(tabella{j,1},tabella{j,2});
+    if(~cellfun('isempty',tabellaPesi(j,1)))
+        tabx = table(tabellaPesi{j,1},tabellaTempi{j,1});
         tab1 = sortrows(tabx,2);
         %%tab2 = groupsummary(tab1,'Var2',hours(4),'median','Var1');
         tab2 = groupsummary(tab1,'Var2',hours(4),@(x) prctile(x,90));
@@ -82,7 +84,7 @@ for j = 1:Rows
         ylabel('Result(ms)') 
         legend(Nations{j})
         set(gcf,'color','w');
-        fname = sprintf('%s_%s_90Perc_%s', FileOut{1},FileOut{3},Nations{j});
+        fname = sprintf('%s_%s_90Perc_%s', FileOut2{end},FileOut{2},Nations{j});
         export_fig(['/home/guazzelli/disco/Thesis/Matlab file/plot/' fname], '-pdf');
     end
 
