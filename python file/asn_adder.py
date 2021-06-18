b271eb16b82ac2fcfa98ce1ddddd894611b85d3d
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-18 18:16:27 +0200

    Update asn_adder.py

--- python file/asn_adder.py
+++ python file/asn_adder.py
@@ -12,14 +12,12 @@ Input2 = sys.argv[2]
 Output= sys.argv[3]
 
 #fieldnames = ['from','fw','group_id','lts','msm_id','msm_name','prb_id','stored_timestamp','timestamp','type','resultset','resultset.af','resultset.dst_addr','resultset.dst_port','resultset.lts','resultset.proto','resultset.result.ANCOUNT','resultset.result.ARCOUNT','resultset.result.ID','resultset.result.NSCOUNT','resultset.result.QDCOUNT','resultset.result.abuf','resultset.result.rt','resultset.result.size','resultset.src_addr','resultset.subid','resultset.submax','resultset.time','mver']
-fieldnames = ['prb_id','timestamp','resultset.result.rt','asn']
 df=pd.read_csv(Input2)
 df1=pd.read_csv(Input,parse_dates=['timestamp'])
-lista=df['prb_id'].tolist()
-lista_asn=df['asn_v4'].tolist()
-inded= df1.index[df1['prb_id'].isin(lista)].tolist()
+#lista=df['prb_id'].tolist()
+#lista_asn=df['asn_v4'].tolist()
+#inded= df1.index[df1['prb_id'].isin(lista)].tolist()
 result=pd.merge(df1,df,how="left", on=["prb_id"])
-
 #print(inded)
 result.drop("Unnamed: 0",axis=1, inplace=True)
 #print(result)
