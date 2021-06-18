4a4faab786ff82dd1aaaab3e8d4055b35c787cfb
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-18 18:04:40 +0200

    Update asn_adder.py

--- python file/asn_adder.py
+++ python file/asn_adder.py
@@ -13,18 +13,15 @@ Output= sys.argv[3]
 
 #fieldnames = ['from','fw','group_id','lts','msm_id','msm_name','prb_id','stored_timestamp','timestamp','type','resultset','resultset.af','resultset.dst_addr','resultset.dst_port','resultset.lts','resultset.proto','resultset.result.ANCOUNT','resultset.result.ARCOUNT','resultset.result.ID','resultset.result.NSCOUNT','resultset.result.QDCOUNT','resultset.result.abuf','resultset.result.rt','resultset.result.size','resultset.src_addr','resultset.subid','resultset.submax','resultset.time','mver']
 fieldnames = ['prb_id','timestamp','resultset.result.rt','asn']
-with open(Input,'r',newline='') as f, open(Output,'a',newline='') as out:
-	df=pd.read_csv(Input2)
-	reader1 = DictReader(f)
-	new = csv.writer(out)
-	new.writerow(fieldnames)
-	for line1 in reader1:
-		if (df['prb_id']==int(line1['prb_id'])).bool:
-			#inded= df.index[df['prb_id'].contains(line1['prb_id'])]
-			
-			mask=df[df['prb_id']==int(line1['prb_id'])].index.values[0]		
-			#print(mask)
-			#print(df.iloc[mask]['prb_id'])
-			new.writerow([line1['prb_id'],line1['timestamp'],line1['resultset.result.rt'],df.iloc[mask]['asn_v4']])
+df=pd.read_csv(Input2)
+df1=pd.read_csv(Input,parse_dates=['timestamp'])
+lista=df['prb_id'].tolist()
+lista_asn=df['asn_v4'].tolist()
+inded= df1.index[df1['prb_id'].isin(lista)].tolist()
+result=pd.merge(df1,df,how="left", on=["prb_id"])
 
+#print(inded)
+result.drop("Unnamed: 0",axis=1, inplace=True)
+print(result)
+result.to_csv(Output, index=False) 
 print("DONE")
