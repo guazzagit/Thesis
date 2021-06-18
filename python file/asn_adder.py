9ad4f2a4e78ba5cb0d1b70bee53eadb08ea2ffa1
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-18 16:56:09 +0200

    Update asn_adder.py

--- python file/asn_adder.py
+++ python file/asn_adder.py
@@ -13,15 +13,18 @@ Output= sys.argv[3]
 
 #fieldnames = ['from','fw','group_id','lts','msm_id','msm_name','prb_id','stored_timestamp','timestamp','type','resultset','resultset.af','resultset.dst_addr','resultset.dst_port','resultset.lts','resultset.proto','resultset.result.ANCOUNT','resultset.result.ARCOUNT','resultset.result.ID','resultset.result.NSCOUNT','resultset.result.QDCOUNT','resultset.result.abuf','resultset.result.rt','resultset.result.size','resultset.src_addr','resultset.subid','resultset.submax','resultset.time','mver']
 fieldnames = ['prb_id','timestamp','resultset.result.rt','asn']
-with open(Input,'r') as f, open(Output,'a') as out:
+with open(Input,'r') as f, open(Output,'a',newline='') as out:
 	df=pd.read_csv(Input2)
 	reader1 = DictReader(f)
 	new = csv.writer(out)
 	new.writerow(fieldnames)
 	for line1 in reader1:
-		if (df['prb_id'] == line1['prb_id']).bool:
-			mask = df['prb_id'] == line1['prb_id']
-			#print(df[mask]['asn_v4'].values[0])
-			new.writerow([line1['prb_id'],line1['timestamp'],line1['resultset.result.rt'],df[mask]['asn_v4'].values])
+		if (df['prb_id']==int(line1['prb_id'])).bool:
+			#inded= df.index[df['prb_id'].contains(line1['prb_id'])]
+			
+			mask=df[df['prb_id']==int(line1['prb_id'])].index.values[0]		
+			#print(mask)
+			#print(df.iloc[mask]['prb_id'])
+			new.writerow([line1['prb_id'],line1['timestamp'],line1['resultset.result.rt'],df.iloc[mask]['asn_v4']astype('int64')])
 
 print("DONE")
