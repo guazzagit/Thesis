b232d1c0eb0c019ae840e1533d4cf3059225f95a
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-06-23 16:50:40 +0200

    Create extract_ASN_population.py

---
+++ python file/extract_ASN_population.py
@@ -0,0 +1,21 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+from lxml import etree
+from bs4 import BeautifulSoup
+from pandas.io.html import read_html
+from selenium import webdriver
+#download table from aspop url 
+#require a chrome or other browser driver to work. in this case chrome
+url='https://stats.labs.apnic.net/aspop/'
+chrome_Driver_path="C:/Users/guazz/Documents/GitHub/Thesis/python file/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_Driver_path)
+driver.get(url)
+
+table = driver.find_element_by_xpath('//div[@id="table_div"]')
+table_html = table.get_attribute('innerHTML')
+
+df = read_html(table_html)[0]
+print (df)
+df.to_csv('asn_number_user.csv',index=False)
+driver.close()
