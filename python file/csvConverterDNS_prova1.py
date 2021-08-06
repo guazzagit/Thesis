e301092bb8e21d36d24c564489ba6aca5b4ddaef
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-08-06 21:57:39 +0200

    Update csvConverterDNS_prova1.py

--- python file/csvConverterDNS_prova1.py
+++ python file/csvConverterDNS_prova1.py
@@ -9,7 +9,7 @@ from flatten_json import flatten
 Input = sys.argv[1]
 Output = sys.argv[2]
 #fieldnames = ['from','fw','group_id','lts','msm_id','msm_name','prb_id','stored_timestamp','timestamp','type','resultset','resultset.af','resultset.dst_addr','resultset.dst_port','resultset.lts','resultset.proto','resultset.result.ANCOUNT','resultset.result.ARCOUNT','resultset.result.ID','resultset.result.NSCOUNT','resultset.result.QDCOUNT','resultset.result.abuf','resultset.result.rt','resultset.result.size','resultset.src_addr','resultset.subid','resultset.submax','resultset.time','mver']
-fieldnames = ['prb_id','timestamp','resultset.result.rt','subid','dst_addr']
+fieldnames = ['prb_id','timestamp','resultset.result.rt','dst_addr','subid']
 with open(Input,'r') as f, open(Output, 'a',newline='') as out:
     new = csv.writer(out)
     new.writerow(fieldnames)
