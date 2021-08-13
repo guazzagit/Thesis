4a66cbfff81d3748be30f52c867be7e0f86bac92
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-08-13 23:01:28 +0200

    Update DnsAddressTypeDestination.py
    maybe a GIOIA

--- python file/DnsAddressTypeDestination.py
+++ python file/DnsAddressTypeDestination.py
@@ -10,7 +10,7 @@ import ipaddress
 from aslookup import get_as_data
 import pyasn
 from ipwhois import IPWhois
-import multiprocessing
+import multiprocessing as mp
 from joblib import Parallel, delayed
 from tqdm import tqdm
 import ipinfo
@@ -22,8 +22,9 @@ import gc
 import dask.dataframe as dd
 from dask.base import compute
 import dask.multiprocessing
+from functools import partial
 dask.config.set(scheduler='processes')
-
+from multiprocesspandas import applyparallel
 
 access_token = '0cc281a1f8ebe8'
 #handler = ipinfo.getHandler(access_token)
@@ -46,7 +47,7 @@ FamousDNS = pd.read_csv("FamousDNS_addr.csv")
 #priv_pub_ip2asn = pd.read_csv("ip2asn-combined.csv")
 #ipv4=pd.read_csv("ip2asn-v4.csv")
 #ipv6=pd.read_csv("ip2asn-v6.csv")
-df = dd.read_csv(Input) #,parse_dates=['timestamp']
+df = pd.read_csv(Input) #,parse_dates=['timestamp']
 #read csv cosi leggiamo tutto da li e via il discorso di asn dei famosi.
 #prb_id,timestamp,resultset.result.rt,dst_addr,subid,country_code,asn_v4
 #fieldnames = ['prb_id','timestamp','resultset.result.rt','dst_addr','country_code','asn_v4','ASN_dest,Type']
@@ -55,38 +56,44 @@ df['Type']=""
 
 
 def myfunc(self):
-
-	if (self['dst_addr'] in FamousDNS['ip'].values):
-		pos=np.where(FamousDNS["ip"]==self['dst_addr'])
-		as_pub=FamousDNS.iloc[pos[0][0],0]
-		ASN_dest= as_pub
-		Type= 'Public'
-
-
-	elif (ipaddress.ip_address(self['dst_addr']).is_private):
-		ASN_dest = self['asn_v4']
-		Type= 'Private'
-	else:
-		try:
-			net = Net(self['dst_addr'])
-			obj = IPASN(net)
-			results = obj.lookup(retry_count=0,asn_methods=['whois'])
-			as_unkn= results['asn']
-			ASN_dest= as_unkn
-			Type= 'UnknownPublic'
-			#print(self['Type'])
-			#writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],self['ASN_dest'],self['Type']))
-
-		except:
-			asnn=float(self['asn_v4'])
-			ASN_dest = asnn
-			Type = 'Private'
-	writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],ASN_dest,Type))
+	with open(Output,"a",newline='') as out:
+		writer = csv.writer(out)
+		if (self['dst_addr'] in FamousDNS['ip'].values):
+			pos=np.where(FamousDNS["ip"]==self['dst_addr'])
+			as_pub=FamousDNS.iloc[pos[0][0],0]
+			ASN_dest= as_pub
+			Type= 'Public'
+			#writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],ASN_dest,Type))
 
 
+		elif (ipaddress.ip_address(self['dst_addr']).is_private):
+			ASN_dest = self['asn_v4']
+			Type= 'Private'
+			#writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],ASN_dest,Type))
 
-def _apply_df(args):
-    df.apply(myfunc,axis=1)
+		else:
+			try:
+				net = Net(self['dst_addr'])
+				obj = IPASN(net)
+				results = obj.lookup(retry_count=0,asn_methods=['whois'])
+				as_unkn= results['asn']
+				ASN_dest= as_unkn
+				#self['ASN_dest']= as_unkn
+				Type= 'UnknownPublic'
+				#print(self['Type'])
+				#writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],self['ASN_dest'],self['Type']))
+
+			except:
+				asnn=float(self['asn_v4'])
+				ASN_dest = asnn
+				Type = 'Private'
+		writer.writerow((self['prb_id'],self['timestamp'],self['resultset.result.rt'],self['dst_addr'],self['country_code'],self['asn_v4'],ASN_dest,Type))
+		out.close()
+
+
+def _apply_df(data_split):
+    return data_split.apply(myfunc,axis=1)
+    
 
 def apply_by_multiprocessing(df,func,**kwargs):
   workers=kwargs.pop('workers')
@@ -95,12 +102,17 @@ def apply_by_multiprocessing(df,func,**kwargs):
   pool.close()
 
 
-	 
 with open(Output,"a",newline='') as out:
 	writer = csv.writer(out)
 	writer.writerow(['prb_id,timestamp,resultset.result.rt,dst_addr,country_code,asn_v4,ASN_dest,Type'])
+	out.close()
 	print('start')
-	df.compute(num_workers=2).apply(myfunc, axis=1)
+	#data_split = np.array_split(df, 10)
+	#pool = mp.Pool(10)	
+	#pool.map(_apply_df, data_split)
+	#pool.close()
+	df.apply_parallel(myfunc, num_processes=30, axis=0)
+	#df.apply(myfunc, axis=1)
 	#df.to_csv(Output, index=False)
 	print("end")
       
