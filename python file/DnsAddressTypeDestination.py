51844dccbfe0561888da116a37c7fd54bea2cdfb
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-08-13 16:12:42 +0200

    Update DnsAddressTypeDestination.py

--- python file/DnsAddressTypeDestination.py
+++ python file/DnsAddressTypeDestination.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 import ipinfo
 from ipwhois.net import Net
 from ipwhois.asn import IPASN
-from pandarallel import pandarallel
+#from pandarallel import pandarallel
 import swifter
 
 access_token = '0cc281a1f8ebe8'
