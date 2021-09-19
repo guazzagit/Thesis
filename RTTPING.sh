21533f844276d4a55ef15d4c84eb0cbb056c6e14
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-09-19 23:22:00 +0200

    Update RTTPING.sh

--- RTTPING.sh
+++ RTTPING.sh
@@ -8,4 +8,4 @@ EXT3=".mat"
 
 
 
-/usr/local/MATLAB/R2020b/bin/matlab -batch system("addpath('github_repo')"; "addpath('/home/guazzelli/disco/file')"; "addpath('/home/guazzelli/disco/Thesis/Matlab file')"; "'/home/guazzelli/disco/Thesis/Matlab file/'PlotEuropePING "$Input1$EXT2" "$Input3$EXT3"")
+/usr/local/MATLAB/R2020b/bin/matlab -batch system("addpath('github_repo') & addpath('/home/guazzelli/disco/file') & addpath('/home/guazzelli/disco/Thesis/Matlab file') & '/home/guazzelli/disco/Thesis/Matlab file/'PlotEuropePING "$Input1$EXT2" "$Input3$EXT3"")
