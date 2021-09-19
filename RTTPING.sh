948e92dc9c7fe27801d4281d10556526fff4d9c4
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-09-19 23:31:11 +0200

    Update RTTPING.sh

--- RTTPING.sh
+++ RTTPING.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 Input1="$1"
 Input2="$2"
-Input3="$3"
-Input4="$4"
 EXT2=".csv"
 EXT3=".mat"
 /usr/local/MATLAB/R2020b/bin/matlab -batch '/home/guazzelli/disco/Thesis/Matlab file/'PlotEuropePING "$Input1$EXT2" "$Input3$EXT3"
