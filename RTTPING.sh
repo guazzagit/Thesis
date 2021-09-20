019eb988f9b2ca410b5bc891f5143b36c39e1758
Stefano <45688605+guazzagit@users.noreply.github.com>
2021-09-20 09:18:35 +0200

    Update RTTPING.sh

--- RTTPING.sh
+++ RTTPING.sh
@@ -4,4 +4,4 @@ Input2="$2"
 EXT2=".csv"
 EXT3=".mat"
 echo '$Input1$EXT2'
-/usr/local/MATLAB/R2020b/bin/matlab -nodisplay -nodesktop "try; /home/guazzelli/disco/Thesis/Matlab file/PlotEuropePING "$Input1$EXT2" "$Input2$EXT3"; catch; end; quit;"
+/usr/local/MATLAB/R2020b/bin/matlab -nodisplay -nodesktop -r "try; /home/guazzelli/disco/Thesis/Matlab file/PlotEuropePING "$Input1$EXT2" "$Input2$EXT3"; catch; end; quit;"
